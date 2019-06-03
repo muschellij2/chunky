823978dd847fb91ddd22daf39731a7d0b50df8d3
muschellij2 <muschellij2@gmail.com>
2019-06-02 18:10:20 -0700

    updated the multiple thing

--- R/nii2fst.R
+++ R/nii2fst.R
@@ -1,34 +1,56 @@
 #' Convert Nifti Image into FST Fast file
 #'
-#' @param x an object that [RNifti::retrieveNifti]
+#' @param file a filename of the image
 #' @param fst_file output filename of FST file
+#' @param read_in Read in the FST file using
+#' [fst::read_fst] and return an FST object rather than a
+#' filename
 #' @param ... additional arguments to [fst::write_fst]
+#'
 #' @importFrom RNifti retrieveNifti
 #' @importFrom fst write_fst read_fst
 #'
-#' @return A file path of the FST file
+#' @return A file path of the FST file or FST data read if
+#' `read_in = TRUE`
 #' @export
 #'
 #' @examples
-#' arr = array(rnorm(50^3), dim = rep(50, 3))
+#' if (requireNamespace("oro.nifti", quietly = TRUE) &
+#'     requireNamespace("neurobase", quietly = TRUE)) {
+#' L = lapply(1:10, function(x) {
+#' arr = oro.nifti::nifti(array(rnorm(50^3), dim = rep(50, 3)))
+#' tfile = tempfile()
+#' neurobase::writenii(arr, tfile)
+#' tfile
+#' })
+#' arr = L[[1]]
 #' nii2fst(arr)
-nii2fst = function(x, fst_file = NULL, ...) {
-  img = RNifti::retrieveNifti(object = x)
+#' nii2fst(arr, read_in = TRUE)
+#' res = nii2fst_mult(L)
+#' res = nii2fst_mult(L, read_in = TRUE)
+#' }
+nii2fst = function(file, fst_file = NULL, read_in = FALSE, ...) {
+  stopifnot(is.character(file))
+  img = RNifti::readNifti(file)
   img = c(img)
   img = data.frame(img = img)
   if (is.null(fst_file)) {
     fst_file = tempfile(fileext = ".fst")
   }
   fst::write_fst(img, path = fst_file, ...)
-  fst_file
+  if (read_in) {
+    return(read_fst(fst_file))
+  } else {
+    return(fst_file)
+  }
 }
 
 
 #' @rdname nii2fst
-check_image_dims = function(x) {
-  x = lapply(x, RNifti::retrieveNifti)
-  d = dim(x[[1]])
-  check = sapply(x, function(r){
+check_image_dims = function(file) {
+  file = lapply(file, RNifti::readNifti)
+  d = dim(file[[1]])
+  check = sapply(file, function(r){
     all(dim(r) == d)
   })
   if (!all(check)) {
@@ -40,9 +62,16 @@ check_image_dims = function(x) {
 
 #' @rdname nii2fst
 #' @export
-nii2fst_par = function(x) {
-  check_image_dims(x)
-  res = lapply(x, nii2fst)
+nii2fst_mult = function(file, ...) {
+  check_image_dims(file)
+  img = RNifti::readNifti(file[[1]])
+  d = dim(img)
+  indices = expand.grid(dim1 = 1:d[1],
+                        dim2 = 1:d[2],
+                        dim2 = 1:d[3])
+  res = lapply(file, nii2fst, ...)
+  res = list(indices = indices,
+             fst_data = res)
   res
 }
 
