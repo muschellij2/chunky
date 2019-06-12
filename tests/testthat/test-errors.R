fb11195effe8a7166844d08c6b2705a252590966
muschellij2 <muschellij2@gmail.com>
2019-06-11 21:41:57 -0400

    updated things

---
+++ tests/testthat/test-errors.R
@@ -0,0 +1,43 @@
+testthat::context("Testing errors")
+
+
+testthat::test_that("Checking 5D example", {
+  if (requireNamespace("oro.nifti", quietly = TRUE) &
+      requireNamespace("neurobase", quietly = TRUE)) {
+
+    nd = rep(10, 5)
+    L = lapply(1:3, function(x) {
+      arr = oro.nifti::nifti(array(rnorm(prod(nd)),
+                                   dim = nd))
+      tfile = tempfile()
+      neurobase::writenii(arr, tfile)
+      tfile
+    })
+    arr = L[[1]]
+    testthat::expect_error(nii2fst(arr),
+                           regexp = "only works")
+    testthat::expect_error(nii2fst(L),
+                           regexp = "only works")
+
+  }
+})
+
+testthat::test_that("Checking 5D with drop example", {
+  if (requireNamespace("oro.nifti", quietly = TRUE) &
+      requireNamespace("neurobase", quietly = TRUE)) {
+    nd = c(rep(10, 4), 1)
+    L = lapply(1:3, function(x) {
+      arr = oro.nifti::nifti(array(rnorm(prod(nd)),
+                                   dim = nd))
+      tfile = tempfile()
+      neurobase::writenii(arr, tfile)
+      tfile
+    })
+    arr = L[[1]]
+    testthat::expect_silent({out = nii2fst(arr)})
+    testthat::expect_is(out$data, "character")
+    testthat::expect_is(out$indices, "fst_table")
+    testthat::expect_is(out, "niifst_table")
+    testthat::expect_true(file.exists(out$data))
+  }
+})
